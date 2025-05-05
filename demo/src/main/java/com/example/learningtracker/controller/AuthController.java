043205900eda2ff82cb9e647c60d72eddf8bae17
bigxuanfeng <1714632509@qq.com>
2025-05-05 20:23:10 +0800

    Add files via upload

---
+++ demo/src/main/java/com/example/learningtracker/controller/AuthController.java
@@ -0,0 +1,34 @@
+package com.example.learningtracker.controller;
+
+import com.example.learningtracker.model.User;
+import com.example.learningtracker.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class AuthController {
+    private final UserService userService;
+
+    public AuthController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(User user) {
+        userService.registerUser(user);
+        return "redirect:/login?registered";
+    }
+}
